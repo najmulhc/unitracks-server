028bcb025bf31cb03d71016f96f43391f8b0065c
Najmul Huda Chowdhury Chayan <najmulhc@outlook.com>
2023-10-31 19:34:00 +0600

    login bug fixed

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -47,7 +47,7 @@ export const login = async (req: Request, res: Response) => {
     if (!user) {
       throw new Error("User not found");
     }
-    const compared = bcrypt.compare(password, user.hashedPassword);
+    const compared = await bcrypt.compare(password, user.hashedPassword);
     if (!compared) {
       throw new Error("Incorrect password!");
     }
