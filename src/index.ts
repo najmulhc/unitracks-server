69a25cc6b1048d7d0520c200fef0daeb58244c74
Najmul Huda Chowdhury Chayan <najmulhc@outlook.com>
2023-10-31 18:57:36 +0600

    cors added

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ const express = require("express")
 // import cookieParser from "cookie-parser";
 import userRouter from "./routes/user.route";
 const dotenv = require("dotenv")
+import cors from 'cors'
 
 const app = express();
 const PORT = process.env.PORT || 6969;
 app.use(express.json());
+app.use(cors())
 // app.use(cookieParser("shhh......."));
 dotenv.config();
 app.use("/user", userRouter);
